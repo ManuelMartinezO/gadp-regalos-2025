b0c3594cd69adf3f75e3cfdf8f761f218bb1e83e
Rafael <rafaelpatricioayllon123@gmail.com>
2025-12-11 16:37:54 -0400

    Filtro de juguetes por dia

--- routes/index.js
+++ routes/index.js
@@ -3,68 +3,6 @@ const router = express.Router();
 const Personal = require('../models/Personal');
 const Juguete = require('../models/Juguete');
 
-/* // Página principal con formulario
-router.get('/', async (req, res) => {
-  try {
-    const personal = await Personal.find();
-    res.render('index', { personal });
-  } catch (error) {
-    res.status(500).send('Error al cargar la página');
-  }
-});
-
-// Registrar personal
-router.post('/personal', async (req, res) => {
-  try {
-    const nuevoPersonal = new Personal(req.body);
-    await nuevoPersonal.save();
-    res.redirect('/');
-  } catch (error) {
-    res.status(500).send('Error al registrar personal');
-  }
-});
-
-// Registrar entrega de juguetes
-router.post('/juguetes', async (req, res) => {
-  try {
-    const nuevoJuguete = new Juguete(req.body);
-    await nuevoJuguete.save();
-    res.redirect('/estadisticas');
-  } catch (error) {
-    res.status(500).send('Error al registrar juguete');
-  }
-});
-
-// Ver estadísticas
-router.get('/estadisticas', async (req, res) => {
-  try {
-    const juguetes = await Juguete.find().populate('personal');
-    
-    // Calcular totales por categoría
-    const totales = await Juguete.aggregate([
-      {
-        $group: {
-          _id: '$categoria',
-          total: { $sum: '$cantidad' }
-        }
-      }
-    ]);
-    
-    const estadisticas = {
-      niño: 0,
-      niña: 0,
-      bebe: 0
-    };
-    
-    totales.forEach(item => {
-      estadisticas[item._id] = item.total;
-    });
-    
-    res.render('estadisticas', { juguetes, estadisticas });
-  } catch (error) {
-    res.status(500).send('Error al cargar estadísticas');
-  }
-}); */
 router.get('/estadisticas', async (req, res) => {
   try {
     let { fecha } = req.query;
